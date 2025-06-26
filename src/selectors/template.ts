daf0ee9d4beae07ca2372ca657bc08386097bf2c
Frederico Guilherme Klüser de Oliveira <kluserhuu@gmail.com>
2025-06-25 23:57:11 -0300

    Add TemplateButtons selector with attributes for gamepad icon

---
+++ src/selectors/template.ts
@@ -0,0 +1,10 @@
+import { TagWithAttributes } from '../findElementByHierarchy';
+
+const TemplateButtons: TagWithAttributes[] = [
+	{
+		tag: 'span',
+		attributes: [{ attribute: 'class', value: 'gamepad-2nd-icon-wrapper' }],
+	},
+];
+
+export default TemplateButtons;
